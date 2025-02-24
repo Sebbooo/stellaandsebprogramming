fa0c18af678175f4cfc77dcd7f7ffaa7a7f1558f
Sebastian Attlan <sattlan@college.harvard.edu>
2025-02-24 12:15:34 -0500

    outline stuff

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
 print("we are amazing")
-print("boboeboab")
+
+# How to represent graph
+
+graph = {}
+
+# Generate graph of 
+def generate_graph(type, size):
+    pass
+
+class Heap:
+    # TBD, I think just start empty
+    def __init__(self):
+        pass
+
+    # Insert blah
+    def insert(elem):
+        pass
+
+    # Return and Remove smallest
+    def pop():
+        pass
+
+# Gives type of random num
+def random(type):
+    pass
+
+
+# Implement testing area
+
