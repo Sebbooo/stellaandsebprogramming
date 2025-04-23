3102aff2541dc8f7ccf548de246ea0aee02202af
StellaL123 <stellali110204@gmail.com>
2025-04-22 20:10:15 -0400

    Add prepartitioned_repeated_random implementation

--- pset3/numberpartition.py
+++ pset3/numberpartition.py
@@ -40,8 +40,47 @@ def calculate_residue(A, S):
     total = sum(A[i] * S[i] for i in range(len(A)))
     return abs(total)
 
+# Prepartitioned Repeated Random
+def prepartitioned_repeated_random(A, max_iter):
+    # Start with a random solution P (prepar)
+    n = len(A)
+    P = [random.randint(1, n) for _ in range(n)]
+    best_residue = calculate_prepartition_residue(A, P)
+    
+    # For iter = 1 to max_iter
+    for _ in range(max_iter):
+        # P' = a random solution
+        P_prime = [random.randint(1, n) for _ in range(n)]
+        residue_prime = calculate_prepartition_residue(A, P_prime)
+        
+        # if residue(P') < residue(P) then P = P'
+        if residue_prime < best_residue:
+            P = P_prime
+            best_residue = residue_prime
+    
+    # returns best assignment found out of all iterations
+    return P
+
+# Calculate the residue for a prepartitioned solution
+def calculate_prepartition_residue(A, P):
+    # create a dictionary to track the sum of each partition
+    partition_sums = {}
+    
+    # calculate the sum for each number
+    for i in range(len(A)):
+        partition = P[i]
+        if partition in partition_sums:
+            partition_sums[partition] += A[i]
+        else:
+            partition_sums[partition] = A[i]
+    
+    # dictionary values to an array
+    partition_array = list(partition_sums.values())
+    
+    # use kk to calculate residue
+    return kk(partition_array)
+
 # Hill Climbing
 # Simulated Annealing
-# Prepartitioned Repeated Random
 # Prepartitioned Hill Climbing
 # Prepartitioned Simulated Annealing
